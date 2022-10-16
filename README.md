6e45d431e9eb5abf7ff6a87a5df208531e446cb8
Soprano <antfar67@gmail.com>
2022-10-16 15:37:48 -0700

    Edited README file to include screenshots, edited conditional statements for various prompts in script

--- README.md
+++ README.md
@@ -2,12 +2,7 @@
 
 ## Description
 
-Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
-
-- What was your motivation?
-- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
-- What problem does it solve?
-- What did you learn?
+I wrote this JavaScript fucntion to generate a random password based on a series of user prompts. I learned how to incorporate alerts, confirms, and prompts into my HTML applications through JavaScript, as well as how to modify arrays and strings through a variety of methods and functions. 
 
 ## Installation
 
@@ -15,25 +10,26 @@ N/A
 
 ## Usage
 
-To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
-
-    ```md
-    ![alt text](assets/images/screenshot.png)
-    ```
-
 1. Access the website at the provided URL: 
-![alt text](assets/images/screenshot.png)
 
 2. Click on "Generate Password"
-![alt text](assets/images/screenshot.png)
+![Start Page](assets/images/start-page.png)
 
 3. Answer the prompts asking for password length, whether to include uppercase letters, numbers, and/or special characters.
-![alt text](assets/images/screenshot.png)
+![Password Length Prompt](assets/images/pw-length.png)
+
+![Uppercase Prompt](assets/images/uppercase.png)
+
+![Numbers Prompt](assets/images/numbers.png)
+
+![Special Prompt](assets/images/special.png)
 
 4. Copy the generated password to your Clipboard for further use.
-![alt text](assets/images/screenshot.png)
+![Random Password](assets/images/password.png)
 
 ## Credits
+
+Collaborator(s):
 
 Dallas Sybrowsky: https://github.com/DallasSybrowsky
 
--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// arrays of usable characters in password
 var numberList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var lowerList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var charList = [" ", "!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
 
+// generates a random password for the writePassword function
 function generatePassword() {
   var passwordLength;
   var includeSpecial;
@@ -19,19 +21,21 @@ function generatePassword() {
   validNumber = false;
   validSpecial = false;
 
+  // while loops ensure users enter in the correct parameters for each prompt
+  // prompts user to write a number between 8 - 128 to determine password length
   while (validLength === false) {
     passwordLength = prompt("How many characters would you like in your password? Please enter a number between 8 and 128.");
-
-    if (passwordLength < 8 || passwordLength > 128) {
+    if (passwordLength < 8 || passwordLength > 128 && isNaN(passwordLength) === false) {
       alert("Please enter a number between 8 and 128");
       validLength = false;
     }
-    else if ([passwordLength >= 8 || passwordLength <= 128]) {
+    else if ([passwordLength >= 8 || passwordLength <= 128] && isNaN(passwordLength) === false) {
       validLength = true;
     }
-    // else if (includeNumber === null){
-    //   break;
-    // }
+    else if (passwordLength === false){
+      validLength = true;
+      break;
+    }
     else {
       alert("Please enter a number between 8 and 128.")
       validLength = false;
@@ -40,26 +44,20 @@ function generatePassword() {
 
   console.log("Your password will be: " + passwordLength + " characters long.");
 
+  // prompts user to choose to include/exclude uppercase characters
   while (validUpper === false) {
     includeUpper = prompt("Would you like to use uppercase characters? Type Yes or No.");
     if (includeUpper === "no" || includeUpper === "No") {
-      // pwCharacters.concat(lowerList);
       validUpper = true;
       pwCharacters = lowerList;
       console.log(pwCharacters);
-      
+
     }
-    else if (includeUpper === "yes" || includeUpper ==="Yes") {
-      // pwCharacters = pwCharacters.concat(lowerList);
-      // pwCharacters = pwCharacters.concat(upperList);
+    else if (includeUpper === "yes" || includeUpper === "Yes") {
       validUpper = true;
-      pwCharacters = pwCharacters.concat(lowerList,upperList)
+      pwCharacters = pwCharacters.concat(lowerList, upperList)
       console.log(pwCharacters);
-      
     }
-    // else if (includeUpper === null){
-    //   break;
-    // }
     else {
       validUpper = false;
     }
@@ -67,7 +65,8 @@ function generatePassword() {
   console.log("Using upper case letters: " + includeUpper + ".");
 
   console.log(pwCharacters);
- 
+
+  // prompts user to choose to include/exclude numbers
   while (validNumber === false) {
     includeNumber = prompt("Would you like to use numbers? Type Yes or No.");
     if (includeNumber === "no" || includeNumber === "No") {
@@ -77,9 +76,6 @@ function generatePassword() {
       pwCharacters = pwCharacters.concat(numberList);
       validNumber = true;
     }
-    // else if (includeNumber === null){
-    //   break;
-    // }
     else {
       validNumber = false;
     }
@@ -89,18 +85,16 @@ function generatePassword() {
 
   console.log(pwCharacters);
 
+  // prompts user to choose to include/exclude special characters
   while (validSpecial === false) {
     var includeSpecial = prompt("Would you like to use special characters? Type Yes or No.");
-    if (includeSpecial === "no" || includeSpecial ===  "No") {
+    if (includeSpecial === "no" || includeSpecial === "No") {
       validSpecial = true;
     }
     else if (includeSpecial === "yes" || includeSpecial === "Yes") {
       pwCharacters = pwCharacters.concat(charList);
       validSpecial = true;
     }
-    // else if (includeSpecial === null){
-    //   break;
-    // }
     else {
       validSpecial = false;
     }
@@ -109,12 +103,13 @@ function generatePassword() {
   console.log("Using special characters: " + includeSpecial + ".");
   console.log(pwCharacters);
 
-
+  // creates a randomly generated character and writes it to an array
   for (var i = 0; i < passwordLength; i++) {
     var randomChar = pwCharacters[Math.floor(Math.random() * pwCharacters.length)];
     pwArray[i] = randomChar;
     console.log(pwArray[i]);
   }
+  // join array into string
   pwString = pwArray.join("");
   console.log(pwString);
   // Return password
@@ -123,11 +118,12 @@ function generatePassword() {
 }
 
 function writePassword() {
+  // password is generated from function and saved in the password variable
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
-
+// password is written to the password generation box on the page
 generateBtn.addEventListener("click", writePassword);
